e5efcc2e8a4f89043c7dd074e953cd727d9b4297
Ramil <crouzch1@gmail.com>
2024-06-11 02:09:59 +0500

    create Feedback component

---
+++ src/components/Feedback/Feedback.tsx
@@ -0,0 +1,39 @@
+import style from './Feedback.module.scss';
+
+import { feedbackData as data } from '../../helpers/constants';
+
+import Info from '../common/Info/Info';
+
+interface FeedbackProps {}
+
+export const Feedback: React.FC<FeedbackProps> = () => {
+	return (
+		<ul>
+			{data.map((item) => (
+				<li className={style.item} key={item.id}>
+					<Info text={item.feedback} className={style.feedbackText} />
+
+					<div className={style.reporterInfo}>
+						<img
+							src={require(`../../assets/images/${item.reporter.photoUrl}`)}
+							alt='reporter_img'
+							className={style.reporterImg}
+						/>
+
+						<p className={style.reporterName}>
+							{`${item.reporter.name}, `}
+							<a
+								href={`${item.reporter.citeUrl}`}
+								className={style.reporterLink}
+							>
+								{item.reporter.citeUrl}
+							</a>
+						</p>
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default Feedback;
