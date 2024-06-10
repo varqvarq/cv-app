4ccf9b4cc04cad5c8081e31a95909ed097c091af
Ramil <crouzch1@gmail.com>
2024-06-11 02:13:50 +0500

    files fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,17 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Inner from './pages/Inner/Inner';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/inner' element={<Inner />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
